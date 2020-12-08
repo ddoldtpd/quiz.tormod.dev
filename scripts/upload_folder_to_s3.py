21ecf99be4d17dd94c320ff9112f1e239cd99c71
Fredrik Tormod <61283883+ddoldtpd@users.noreply.github.com>
2020-12-08 15:55:34 +0100

    testing

--- scripts/upload_folder_to_s3.py
+++ scripts/upload_folder_to_s3.py
@@ -30,7 +30,7 @@ def main():
         for root, dirs, files in os.walk(local_path):
             for file in files:
 
-                s3_path = "." + os.path.join(root, file).rsplit("build", 1)[1]
+                s3_path = os.path.join(root, file).rsplit("build/", 1)[1]
                 print(s3_path)
                 print(f"root: {os.path.join(root, file)}")
                 contentType = magic.from_file(os.path.join(root, file), mime=True)
