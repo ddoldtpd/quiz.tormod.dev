d787c2e971e2b755e046d5f0d12b7fbbe5192652
Fredrik Tormod <61283883+ddoldtpd@users.noreply.github.com>
2020-12-08 16:57:34 +0100

    testing

--- scripts/upload_folder_to_s3.py
+++ scripts/upload_folder_to_s3.py
@@ -42,20 +42,21 @@ def main():
         for root, dirs, files in os.walk(local_path):
             for file in files:
 
-                s3_path = os.path.join(root, file).rsplit("build/", 1)[1]
+                file_origin = os.path.join(root, file)
+                s3_path = file_origin.rsplit("build/", 1)[1]
 
                 print(s3_path)
-                print(f"root: {os.path.join(root, file)}")
-                contentType = magic.from_file(os.path.join(root, file), mime=True)
+                print(f"root: {file_origin}")
+                contentType = magic.from_file(file_origin, mime=True)
                 contentType = setContentType(file, contentType)
                 print(f"ContentType is {contentType} for file {s3_path}")
                 extra_args = {"ContentType": f"{contentType}",
                               "ACL": "public-read"}
 
-                print(f"s3: {s3}, file: {os.path.join(root, file)}, bucket: {bucket_name}, s3_path: {s3_path}, extra_args:{extra_args}")
+                print(f"s3: {s3}, file: {file_origin}, bucket: {bucket_name}, s3_path: {s3_path}, extra_args:{extra_args}")
                 uploadDirectory(
                     s3,
-                    os.path.join(root, file),
+                    file_origin,
                     bucket_name,
                     s3_path,
                     extra_args
