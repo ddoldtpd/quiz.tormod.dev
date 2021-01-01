1dfbf2c535e4f7bb3dd0364944e7524bea998d69
Fredrik Tormod <61283883+ddoldtpd@users.noreply.github.com>
2021-01-01 15:53:25 +0100

    Build for production..

--- build/precache-manifest.b317cd0a83c2672d8c0d27c4b832cb44.js
+++ build/precache-manifest.b317cd0a83c2672d8c0d27c4b832cb44.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "63e9cbaccddaf1b944e5dfbd589ddba5",
+    "revision": "32121107663a40eef113b672ef6c339f",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.6414bb7b.chunk.css"
   },
   {
-    "revision": "14df7d5b594681326833",
+    "revision": "447c127c6b4ec401653a",
     "url": "/static/css/main.1d0f4122.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.8f3893c0.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "14df7d5b594681326833",
-    "url": "/static/js/main.e1c4f476.chunk.js"
+    "revision": "447c127c6b4ec401653a",
+    "url": "/static/js/main.7307689f.chunk.js"
   },
   {
     "revision": "218cdb7c2f34a21f7fdb",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.76bf1ad134e7d1110c9717cc5f8efe48.js"
+  "/precache-manifest.b317cd0a83c2672d8c0d27c4b832cb44.js"
 );
 
 self.addEventListener('message', (event) => {
